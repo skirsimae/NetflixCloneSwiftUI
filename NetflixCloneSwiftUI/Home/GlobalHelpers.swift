d499fb40c9a60885f35b378bff6c18e1dd9c9ead
Silva Kirsimae <silvakirsimae@gmail.com>
2023-03-03 11:55:47 +0100

    Add GlobalHelpers file to feed previews data

---
+++ NetflixCloneSwiftUI/Home/GlobalHelpers.swift
@@ -0,0 +1,10 @@
+//
+//  GlobalHelpers.swift
+//  NetflixCloneSwiftUI
+//
+//  Created by Silva Kirsimae on 03/03/2023.
+//
+
+import Foundation
+
+let exampleMovie1 = Movie(id: UUID().uuidString, name: "DARK", thumbnailURL: URL(string: "https:/picsum.photos/200/300")!)
