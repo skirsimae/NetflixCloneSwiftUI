3c0a014ae01fb3ebe6dd4e01ceecbabc2df9567c
Silva Kirsimae <silvakirsimae@gmail.com>
2023-03-03 16:01:15 +0100

    Add Movie details screen file
    Add MovieInfoSubheadline

---
+++ NetflixCloneSwiftUI/MovieDetail.swift
@@ -0,0 +1,70 @@
+//
+//  MovieDetail.swift
+//  NetflixCloneSwiftUI
+//
+//  Created by Silva Kirsimae on 03/03/2023.
+//
+
+import SwiftUI
+
+struct MovieDetail: View {
+    var movie: Movie
+    
+    let screen = UIScreen.main.bounds
+    
+    var body: some View {
+        ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                HStack {
+                    Spacer()
+                    
+                    Button {
+                        // close the view
+                    } label: {
+                        Image(systemName: "xmark.circle")
+                            .font(.system(size: 28))
+                    }
+                }.padding(.horizontal, 22)
+                
+                ScrollView(.vertical, showsIndicators: false) {
+                    VStack {
+                        StandardHomeMovie(movie: movie)
+                            .frame(width: screen.width / 2.5)
+                        
+                        MovieInfoSubheadline(movie: movie)
+                    }
+                }
+                Spacer()
+            }
+            .foregroundColor(.white)
+        }
+    }
+}
+
+struct MovieDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieDetail(movie: exampleMovie3)
+    }
+}
+
+struct MovieInfoSubheadline: View {
+    var movie: Movie
+    
+    var body: some View {
+        HStack {
+            Image(systemName: "hand.thumbsup.fill")
+                .foregroundColor(.white)
+            
+            Text("MOVIE YEAR")
+            
+            Text("RATING")
+            
+            Text("SEASONS")
+        }
+        .foregroundColor(.gray)
+        .padding(.vertical, 6)
+    }
+}
