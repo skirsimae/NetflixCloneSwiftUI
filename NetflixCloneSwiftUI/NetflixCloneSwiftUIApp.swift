ddd6d662538c8c7d43d7cf746a2abd8f0b5a3ac9
Silva Kirsimae <silvakirsimae@gmail.com>
2023-03-03 10:38:15 +0100

    Initial Commit

---
+++ NetflixCloneSwiftUI/NetflixCloneSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixCloneSwiftUIApp.swift
+//  NetflixCloneSwiftUI
+//
+//  Created by Silva Kirsimae on 03/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct NetflixCloneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
