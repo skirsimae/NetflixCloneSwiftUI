59be0c294b7d06353002cd30f40eea5c2aba0e91
Silva Kirsimae <silvakirsimae@gmail.com>
2023-03-03 13:55:54 +0100

    Set up HomeView with scrollable views

--- NetflixCloneSwiftUI/Home/HomeView.swift
+++ NetflixCloneSwiftUI/Home/HomeView.swift
@@ -8,12 +8,49 @@
 import SwiftUI
 
 struct HomeView: View {
+    var viewModel = HomeViewModel()
+    
     var body: some View {
-        Text("Hello, Netflix!")
+        ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            
+
+            //Main VStack
+            //Lazy VStack will request the images on scroll
+            ScrollView(showsIndicators: false) {
+                LazyVStack {
+                    //identify each category with the id: \.self == each category is unique with the hash value
+                    ForEach(viewModel.allCategories, id: \.self) { category in
+                        VStack {
+                            HStack {
+                                Text(category)
+                                    .font(.title3)
+                                    .bold()
+                                Spacer()
+                            }
+                            
+                            ScrollView(.horizontal, showsIndicators: false) {
+                                HStack {
+                                    ForEach(viewModel.getMovie(for: category)) { movie in
+                                        StandardHomeMovie(movie: movie)
+                                            .frame(width: 100, height: 200)
+                                            .padding(.horizontal, 20)
+                                        
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        .foregroundColor(.white)
     }
 }
 
-struct HomeView_Previews: PreviewProvider {
+struct HomeView_Previews:
+    PreviewProvider {
     static var previews: some View {
         HomeView()
     }
