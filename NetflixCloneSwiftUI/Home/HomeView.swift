85a1588da23bf2932937452fdd74d302bec85b3d
Silva Kirsimae <silvakirsimae@gmail.com>
2023-03-03 10:54:48 +0100

    Add HomeView

---
+++ NetflixCloneSwiftUI/Home/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  NetflixCloneSwiftUI
+//
+//  Created by Silva Kirsimae on 03/03/2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, Netflix!")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
