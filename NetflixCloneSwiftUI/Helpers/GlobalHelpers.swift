adbdaa62b6f8125beba78b0facd3bc4fe41e3746
Silva Kirsimae <silvakirsimae@gmail.com>
2023-03-14 13:20:45 +0100

    Build out season picker and list of episodes barebones

--- NetflixCloneSwiftUI/Helpers/GlobalHelpers.swift
+++ NetflixCloneSwiftUI/Helpers/GlobalHelpers.swift
@@ -25,6 +25,51 @@ let exampleTrailer3 = Trailer(name: "The Mysterious", videoURL: exampleVideoURL,
 
 let exampleTrailers = [exampleTrailer1, exampleTrailer2, exampleTrailer3]
 
+let episode1 = Episode(name: "Beginnings and Endings",
+                       season: 1,
+                       episodeNumber: 1,
+                       thumbnailImageURLString: "https://picsum.photos/300/112",
+                       description: "Six months after the disappearances, the police form a task force. In 2052, Jonas learns that most of Winden perished in an apocalyptic event.",
+                       length: 53,
+                       videoURL: exampleVideoURL)
+let episode2 = Episode(name: "Dark Matter",
+                       season: 1,
+                       episodeNumber: 2,
+                       thumbnailImageURLString: "https://picsum.photos/300/111",
+                       description: "Clausen and Charlotte interview Regina. The Stranger takes Hannah to 1987, where Claudia has an unnerving encounter and Egon visits an old nemesis.",
+                       length: 54,
+                       videoURL: exampleVideoURL)
+let episode3 = Episode(name: "Ghosts",
+                       season: 1,
+                       episodeNumber: 3,
+                       thumbnailImageURLString: "https://picsum.photos/300/110",
+                       description: "In 1954, a missing Helge returns, but he'll only speak to Noah. In 1987, Claudia brings the time machine to Tannhaus, and Egon questions Ulrich again.",
+                       length: 56,
+                       videoURL: exampleVideoURL)
+let episode4 = Episode(name: "Beginnings and Endings",
+                         season: 2,
+                         episodeNumber: 1,
+                         thumbnailImageURLString: "https://picsum.photos/300/109",
+                         description: "Six months after the disappearances, the police form a task force. In 2052, Jonas learns that most of Winden perished in an apocalyptic event.",
+                         length: 53,
+                         videoURL: exampleVideoURL)
+let episode5 = Episode(name: "Dark Matter",
+                         season: 2,
+                         episodeNumber: 2,
+                         thumbnailImageURLString: "https://picsum.photos/300/108",
+                         description: "Clausen and Charlotte interview Regina. The Stranger takes Hannah to 1987, where Claudia has an unnerving encounter and Egon visits an old nemesis.",
+                         length: 54,
+                         videoURL: exampleVideoURL)
+let episode6 = Episode(name: "Ghosts",
+                         season: 2,
+                         episodeNumber: 3,
+                         thumbnailImageURLString: "https://picsum.photos/300/107",
+                         description: "In 1954, a missing Helge returns, but he'll only speak to Noah. In 1987, Claudia brings the time machine to Tannhaus, and Egon questions Ulrich again.",
+                         length: 56,
+                         videoURL: exampleVideoURL)
+
+var allExampleEpisodes = [episode1, episode2, episode3, episode4, episode5, episode6]
+
 let exampleMovie1 = Movie(id: UUID().uuidString, name: "DARK", thumbnailURL: URL(string: "https:/picsum.photos/200/300")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"], year: 2020, rating: "TV-MA", numberOfSeasons: 1, defaultEpisodeInfo: exampleEpisodeInfo1, creators: "SK", cast: "MS", moreLikeThisMovies: [exampleMovie2, exampleMovie3, exampleMovie4, exampleMovie5, exampleMovie6], trailers: exampleTrailers)
 let exampleMovie2 = Movie(id: UUID().uuidString, name: "Travellers", thumbnailURL: URL(string: "https:/picsum.photos/200/300/")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"], year: 2020, rating: "TV-MA", numberOfSeasons: 2, defaultEpisodeInfo: exampleEpisodeInfo1, creators: "SK", cast: "MS", moreLikeThisMovies: [], trailers: exampleTrailers)
 let exampleMovie3 = Movie(id: UUID().uuidString, name: "Community", thumbnailURL: URL(string: "https:/picsum.photos/200/301")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"], year: 2020, rating: "TV-MA", numberOfSeasons: 3, defaultEpisodeInfo: exampleEpisodeInfo1, creators: "SK", cast: "MS", moreLikeThisMovies: [], trailers: exampleTrailers)
