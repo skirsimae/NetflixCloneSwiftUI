155bb32fc4096e2e10f8863bdf446c69137e1cc3
Silva Kirsimae <silvakirsimae@gmail.com>
2023-03-24 16:44:48 +0100

    Handle the logic of loading/showing the X button and hiding the search bar

--- NetflixCloneSwiftUI/Search/SearchBar.swift
+++ NetflixCloneSwiftUI/Search/SearchBar.swift
@@ -9,42 +9,68 @@ import SwiftUI
 
 struct SearchBar: View {
     
-    @State private var text: String = ""
+    @Binding var text: String
+    @Binding var isLoading: Bool
+    
+    @State private var isEditing = true
     
     var body: some View {
         ZStack(alignment: .leading) {
             Color.graySearchBackground
-                .frame(width: 290, height: 36)
+                .frame(width: 270, height: 36)
                 .cornerRadius(8)
-                
+            
             HStack {
                 Image(systemName: "magnifyingglass")
                     .foregroundColor(Color.graySearchText)
                     .padding(.leading, 10)
                 
                 TextField("Search", text: $text)
+                    .padding(7)
+                    .padding(.leading, -7)
+                    .padding(.horizontal, 10)
                     .background(Color.graySearchBackground)
                     .cornerRadius(8)
                     .foregroundColor(.white)
-                    .padding(7)
-                    .padding(.leading, -7)
-                
-                Button {
-                    //Clear text
-                } label: {
-                    Image(systemName: "xmark.circle.fill")
-                        .foregroundColor(.graySearchText)
+                    .onTapGesture {
+                        isEditing = true
+                    }
+                if !text.isEmpty {
+                    if isLoading {
+                        Button {
+                            text = ""
+                        } label: {
+                            ActivityIndicator(style: .medium, animate: .constant(true))
+                                .configure {
+                                    $0.color = .white
+                                }
+                        }
+                        .padding(.trailing, 32)
                         .frame(width: 35, height: 35)
+                        
+                    } else {
+                        Button {
+                            text = ""
+                        } label: {
+                            Image(systemName: "xmark.circle.fill")
+                                .foregroundColor(.graySearchText)
+                                .frame(width: 35, height: 35)
+                        }
+                        .padding(.trailing, 18)
+                    }
                 }
-                .padding(.trailing, 18)
-                
-                Button {
-                    //Clear text, hide both buttons, give up first responder
-                } label: {
-                    Text("Cancel")
-                        .foregroundColor(.white)
+                    
+                if isEditing {
+                    Button {
+                        text = ""
+                        isEditing = false
+                        hideKeyboard()
+                    } label: {
+                        Text("Cancel")
+                            .foregroundColor(.white)
+                    }
+                    .padding(.trailing, 10)
                 }
-                .padding(.trailing, 10)
             }
         }
     }
@@ -56,7 +82,7 @@ struct SearchBar_Previews: PreviewProvider {
             Color.black
                 .edgesIgnoringSafeArea(.all)
             
-            SearchBar()
+            SearchBar(text: .constant(""), isLoading: .constant(false))
                 .padding()
         }
     }
