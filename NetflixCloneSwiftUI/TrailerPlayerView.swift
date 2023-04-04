f9542b241cca00ff81cd0e95990faecbb8206cce
Silva Kirsimae <silvakirsimae@gmail.com>
2023-04-04 15:56:16 +0200

    Add TrailerPlayerView to play a trailer

---
+++ NetflixCloneSwiftUI/TrailerPlayerView.swift
@@ -0,0 +1,29 @@
+//
+//  TrailerPlayerView.swift
+//  NetflixCloneSwiftUI
+//
+//  Created by Silva Kirsimae on 04/04/2023.
+//
+
+import SwiftUI
+import VideoPlayer
+
+struct TrailerPlayerView: View {
+    var videoURL: URL?
+    
+    @Binding var playVideo: Bool
+    
+    var body: some View {
+        if videoURL != nil {
+            VideoPlayer(url: videoURL!, play: $playVideo)
+        } else {
+            Text("Could not load video")
+        }
+    }
+}
+
+struct TrailerPlayerView_Previews: PreviewProvider {
+    static var previews: some View {
+        TrailerPlayerView(videoURL: nil, playVideo: .constant(true))
+    }
+}
